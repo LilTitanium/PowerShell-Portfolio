5890d83d6f48e8d05e2e80a425980c5ee9150227
LilTitanium <87715953+LilTitanium@users.noreply.github.com>
2022-11-04 07:54:17 -0700

    Add files via upload

---
+++ File Search script.ps1
@@ -0,0 +1,30 @@
+﻿# Author: Joseph Sanchez
+# Date: 9/26/2022
+# Updated Date: 11/4/2022
+
+
+# Purpose: Searches through every folder at the specified folder location.
+
+
+#################################################
+
+$Directory = "C:\users\SanchezJ\Downloads" # <--- Enter the path of where the desire folder resides.
+
+cd $Directory #<-- moves to a folder path that is specified above.
+$Folders = Get-ChildItem -File -Recurse | Format-Table # <--- This command will search for everything that it can find in a given folder location.
+
+$Search = { 
+Foreach ($file in $Directory) {
+
+$Folders
+
+}
+}
+& $Search
+
+# Message prompt will display for 10 seconds and then will disappear automatically.
+$Timer = {
+$prompt = New-Object -ComObject wscript.shell
+$message = $prompt.popup("Folder search complete", 10)
+}
+& $Timer
