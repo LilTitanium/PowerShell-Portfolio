6ba39e2d981f3145d41b5b2db8269d2e2abe731e
LilTitanium <87715953+LilTitanium@users.noreply.github.com>
2023-04-22 16:51:56 -0700

    Add files via upload
    I have built this script to compare the hash value of files to ensure data integrity. I have coded this script to grab the file hash of each file, but the best part is that once it is executed it will continue going through the files and the end user can walk away from their computer. This is script is extremely useful, especially when a user is in a situation where they have to leave for the day, but needs to comb through several gigabytes of files. The script will run overnight, and by the time the end user arrives the next morning, the comparison task is completed.

---
+++ PowerShell/FileHashComparison(AllFolders).ps1
@@ -0,0 +1,53 @@
+﻿ #Get-ChildItem $DestinationDirectory -Directory -Exclude 'usss_closed_special_cases', 'Usss_closed_fact_finder_cases'|Format-Table
+$Directory = "E:"
+
+$DestinationDirectory = "E:"
+
+$SourceStorageFiles = "Source storage files"
+$DestinationStorageFiles = "Destination storage files"
+$SourceStorageFolders = "Source Storage folders"
+$DestinationStorageFolders = "Destination storage folders"
+
+$Compare = {
+Start-Transcript -OutputDirectory "C:\users\jsanc\downloads" -IncludeInvocationHeader -Verbose #<-- where "C:\users\sanchezj\desktop\Everything" enter location where you want transcript file to be stored.
+
+Write-Host "=============File Hash Comparison========================"
+
+Get-ChildItem -Recurse -Path $DestinationDirectory | foreach {Get-FileHash -Path $_.FullName} | Format-Table -Property hash, path
+
+Get-ChildItem -recurse -Path $Directory | foreach {Get-FileHash -Path $_.FullName}| Format-Table -Property hash, path
+(Compare-Object -ReferenceObject $Directory -DifferenceObject $DestinationDirectory -Property hash -PassThru).Path 
+
+}
+
+ & $Compare | write-Output | Tee-Object -FilePath "C:\users\jsanc\downloads\Demo.csv" -Verbose #<-- where "C:\users\sanchezj\desktop\Everything" is enter storage location where you want csv file to be stored.
+
+
+$Timer = {
+Write-Host "File Hash Comparison Complete."
+$prompt = New-Object -ComObject wscript.shell
+$message = $prompt.popup("File Hash comparison complete", 10)
+}
+& $Timer
+
+$count = {
+
+Write-Host "=============Files & Folders Comparison========================"
+
+
+Write-Host $SourceStorageFolders (dir $Directory -Directory -Recurse | measure).Count
+Write-Host $DestinationStorageFolders (dir $DestinationDirectory -Directory -Recurse | measure).Count
+
+Write-Host $SourceStorageFiles (dir $Directory -File -Recurse| measure).Count
+Write-Host $DestinationStorageFiles (dir $DestinationDirectory -File -Recurse | measure).Count
+}
+& $count
+
+$Timer = {
+Write-Host "Files and folder Count Complete."
+$prompt = New-Object -ComObject wscript.shell
+$message = $prompt.popup("File and folder count complete", 10)
+}
+& $Timer
+
+Stop-Transcript
