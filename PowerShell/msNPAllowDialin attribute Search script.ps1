0a366dcb2f0fcd0f284210702b367ab69db2123a
LilTitanium <87715953+LilTitanium@users.noreply.github.com>
2023-07-21 22:19:54 -0700

    Add files via upload
    This script searches for the attribute "MsNPAllowDialin" for each user account within Active Directory.  I created this script because we had a user who could not log into their computer with their PIV card because the attribute "MsNPAllowDialin" was set to false. One of my colleagues, Arthuro Saalas switched the attribute to true, then the user was able to sign into their computer.  Arturo has asked me if there was a PowerShell script that would show all the user accounts that were set to false for the msNPAllowDialin attribute.

---
+++ PowerShell/msNPAllowDialin attribute Search script.ps1
@@ -0,0 +1,24 @@
+﻿#Project: Attribute Search Script
+# Author: Joseph Sanchez, IT Cybersecurity Specialist
+
+# Purpose: This script searches for the attribute of "msNPAllowDialin" within Active Directory.
+# The reason why I have built this script is because there was a user that had PIV card issue when they tried to login into their computer.
+# I have searched online,to develop a script that will search all user accounts that had attribute of "msNPAllowDialin" that were set to false.
+
+#Note: You need to have Active Directory Powershell modules installed on your computer in order for the command "Get-ADUser" to work.
+#You can get the Active Directory Powershell modules from RSAT (Remote Server Administration Tool) from Microsoft's website.
+
+################################################################################
+
+
+$users = Get-ADUser -filter {msNPAllowDialin -like "*"} -Properties msNPAllowDialin
+
+if ($users.count -eq 0){
+    Write-Host "No users found with the msNPAllowDialin attribute."
+} else {
+    Write-Host "Users found with the msNPAllowDialin attribute:"
+    foreach ($user in $users){
+        $userDialinStatus = if ($user.msNPAllowDialin) {"Allow"} else {"Not Allowed"}
+        Write-Host "User: $($user.SamAccountName), Dial-in Status: $userDialinStatus"
+    }
+}
